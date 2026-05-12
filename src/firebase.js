c8efd7b4a9ba55b7eef551a29fa50e5b48069974
OFF manual <sundoo.energy3@gmail.com>
2026-05-12 11:01:35 +0900

    Move Firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqk5tpU642WnqOo0AbdJ2ECiKnpuaQZ6w",
-  authDomain: "sundoo-off.firebaseapp.com",
-  projectId: "sundoo-off",
-  storageBucket: "sundoo-off.firebasestorage.app",
-  messagingSenderId: "897062719374",
-  appId: "1:897062719374:web:e1496bd27273d0e3e4dedf",
-  measurementId: "G-F40WWLC98T"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app)
+export default app;
